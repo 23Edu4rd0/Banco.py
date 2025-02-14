30876f031f3b717d9397a6000b9a58c8fac4b14d
23Edu4rd0 <23eduardoviana@gmail.com>
2025-02-14 16:53:26 -0300

    add: adiciona classe Banco para gerenciar saldo e operações

--- Banco.py
+++ Banco.py
@@ -2,6 +2,8 @@ from Deposito import Depositar
 from Sacar import Sacar
 from Extrato_total import extrato
 from historico_global import historico
+from services.cadastro import cadastrar_cliente, cadastrar_conta
+
 
 class Banco:
     def __init__(self, saldo=0.0):
@@ -9,6 +11,23 @@ class Banco:
         self.historico = historico
         self.depositar = Depositar(self.saldo, self.historico)
         self.sacar = Sacar(self.saldo, self.historico)
+        self.clientes = {}
+        self.contas = {}
+
+    def cadastrar_cliente(self):
+        cadastrar_cliente(self.clientes)
+
+    def cadastrar_conta(self):
+        cadastrar_conta(self.contas, self.clientes)
+
+    def login(self):
+        cpf = input("Digite seu CPF para fazer login: ")
+        if cpf not in self.clientes:
+            print("Erro: Cliente não encontrado! Você precisa se cadastrar primeiro.")
+            return None
+        else:
+            print(f"Login realizado com sucesso para o cliente {self.clientes[cpf].nome}.")
+            return cpf
 
     def extrato(self):
         extrato(self.historico, self.saldo)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Banco import Banco
 banco = Banco()
 
 MENU = """
+    [c] Cadastrar Cliente
+    [l] Trocar de Conta
     [d] Depositar
     [s] Sacar
     [e] Extrato
@@ -11,9 +13,30 @@ MENU = """
 
 print("Bem-vindo ao banco Dudu")
 
-while True:
+
+usuario_logado = None
+
+while not usuario_logado:
+    print("\nPor favor, faça login ou se cadastre.")
+    escolha_inicial = input("[l] Login ou [c] Cadastrar Cliente: ").lower()
+
+    if escolha_inicial == "l":
+        usuario_logado = banco.login()
+    elif escolha_inicial == "c":
+        banco.cadastrar_cliente()
+    else:
+        print("Opção inválida. Tente novamente.")
+
+
+while usuario_logado:
     escolha = input(MENU).lower()
-    if escolha == "d":
+
+
+    if escolha == "c":
+        banco.cadastrar_cliente()
+    elif escolha == "l":
+        banco.login()
+    elif escolha == "d":
         banco.depositar_fundo()
     elif escolha == "s":
         banco.sacar_fundo()
